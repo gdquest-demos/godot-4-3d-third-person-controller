ce99bf0e4525803668ed10b6044c5a910d98a02e
Henrique Alves <henriquelalves@gmail.com>
2022-10-12 12:22:36 -0300

    Add lerp to camera height change

--- CameraController.gd
+++ CameraController.gd
@@ -37,7 +37,7 @@ func _process(delta: float) -> void:
 	
 	# Set camera controller to current ground level for the character
 	var target_position = _anchor.global_position + _offset
-	target_position.y = _anchor._ground_height
+	target_position.y = lerp(global_position.y, _anchor._ground_height, 0.1)
 	global_position = target_position
 	
 	# Rotates camera using euler rotation
