3a99b6fbd1b44ca23fcd0c5d9bca927da792ac02
Your Name <gtibo.contact@gmail.com>
2022-10-17 17:45:19 +0200

    Added gdbot model

--- Player/CharacterSkin.gd
+++ Player/CharacterSkin.gd
@@ -1,12 +1,35 @@
 class_name CharacterSkin
-extends MeshInstance3D
+extends Node3D
 
+var animation_tree
+var state_machine
+var moving_blend_path = "parameters/StateMachine/move/blend_position"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# False : set animation to "idle"
+# True : set animation to "move"
+var moving : bool = false : set = set_moving
 
+# Blend value between the walk and run cycle
+# 0.0 walk - 1.0 run
+var move_speed : float = 0.0 : set = set_moving_speed
 
-# Public functions to change animation and etc
-# func jump() -> void:
-# func move(direction: Vector3, speed: float) -> void:
+func _ready():
+	animation_tree = $AnimationTree
+	state_machine = animation_tree.get("parameters/StateMachine/playback")
+	
+func set_moving(value : bool):
+	moving = value
+	if moving:
+		state_machine.travel("move")
+	else:
+		state_machine.travel("idle")
+	
+func set_moving_speed(value : float):
+	move_speed = clamp(value, 0.0, 1.0)
+	animation_tree.set(moving_blend_path, move_speed)
+	
+func jump():
+	state_machine.travel("jump")
+
+func fall():
+	state_machine.travel("fall")
