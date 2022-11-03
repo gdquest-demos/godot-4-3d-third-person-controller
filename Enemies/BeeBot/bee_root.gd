d8d5460e28ba53e01daa3bb80b86eeeed043b8bc
Your Name <gtibo.contact@gmail.com>
2022-11-03 16:35:07 +0100

    Added bee bot

---
+++ Enemies/BeeBot/bee_root.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@onready var state_machine = $AnimationTree["parameters/StateMachine/playback"]
+
+func _ready():
+	play_idle()
+
+func play_idle():
+	state_machine.travel("play_idle")
+
+func play_spit_attack():
+	state_machine.travel("spit_attack")
+
+func play_poweroff():
+	state_machine.travel("power_off")
