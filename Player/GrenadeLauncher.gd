83d31a2fd4e387733f7dbbf790219fa7f4ca0c51
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2022-12-24 22:23:34 +0100

    remove need for camera ref, fix drawing curve

--- Player/GrenadeLauncher.gd
+++ Player/GrenadeLauncher.gd
@@ -1,4 +1,3 @@
-@tool
 class_name GrenadeLauncher
 extends Node3D
 
@@ -8,7 +7,6 @@ const OUT_OF_RANGE_COLOR := Color(0.95, 0.0, 0.17)
 const ENEMY_AIM_COLOR := Color(1, 0, 0, 0.5)
 const SHADER_PARAM_FILL_COLOR := "shader_parameter/fill_color"
 
-@export var camera: Camera3D = null
 @export var max_throw_radius := 10.0
 @export var min_throw_strength := 4.0
 @export var max_throw_strength := 14.0
@@ -19,7 +17,7 @@ var _throw_velocity := Vector3.ZERO
 @onready var _aim_sprite: MeshInstance3D = $AimSprite
 @onready var _grenade_path: Path3D = $LaunchPoint/Path3D
 @onready var _csg_polygon: CSGPolygon3D = $LaunchPoint/Path3D/CSGPolygon3D
-@onready var _raycast: RayCast3D = $RayCast3D
+@onready var _raycast: RayCast3D = $LaunchPoint/RayCast3D
 @onready var _launch_point: Marker3D = $LaunchPoint
 
 
@@ -29,12 +27,6 @@ func _ready() -> void:
 
 	_aim_sprite.material_override.set(SHADER_PARAM_FILL_COLOR, IN_RANGE_COLOR)
 	_csg_polygon.material.set(SHADER_PARAM_FILL_COLOR, IN_RANGE_COLOR)
-
-
-func _get_configuration_warnings() -> PackedStringArray:
-	if camera == null:
-		return PackedStringArray(["This node must have a valid reference to a Camera3D node to orient itself with the camera."])
-	return PackedStringArray()
 
 
 func _physics_process(delta: float) -> void:
@@ -50,29 +42,31 @@ func throw_grenade() -> bool:
 	var grenade := GRENADE_SCENE.instantiate()
 	get_parent().add_child(grenade)
 	# Add small vertical correction to avoid spawning the grenade under the floor
-	grenade.global_position = _grenade_path.global_position + Vector3.UP * 0.1
+	grenade.global_position = _launch_point.global_position
 	grenade.throw(_throw_velocity)
 	return true
 
 
 func update_aim() -> void:
+	var camera := get_viewport().get_camera_3d()
 	var global_camera_look_position := camera.global_position + camera.basis * Vector3.FORWARD * max_throw_radius
 	
-	_raycast.global_position = _launch_point.global_position
 	_raycast.target_position = global_camera_look_position - _raycast.global_position
-	_raycast.force_raycast_update()
 	
 	# TODO: Placing aim reticle on ground: ray aiming down from target position
+	# Or remove reticle unless snapping to target?
 
 	# Snap grenade land position to an enemy the player's aiming at, if applicable
 	var to_target := _raycast.target_position
 	var collider := _raycast.get_collider()
-	_aim_sprite.visible = collider != null
 	if collider:
 		if collider.is_in_group("targeteables"):
+			_aim_sprite.visible = true
 			to_target = collider.global_position - _launch_point.global_position
 		_aim_sprite.global_position = _launch_point.global_position + to_target
 		_aim_sprite.look_at(_launch_point.global_position)
+	else:
+		_aim_sprite.visible = false
 
 	# Calculate the initial velocity the grenade needs based on where we want it to land and how
 	# high the curve should go.
@@ -97,7 +91,7 @@ func update_aim() -> void:
 
 	# Redraw the grenade's motion preview
 	_grenade_path.curve.clear_points()
-	const TIME_STEP := 0.2
+	const TIME_STEP := 0.1
 	var time_current := 0.0
 	while time_current < time_to_land:
 		var point := _throw_velocity * time_current + Vector3.DOWN * gravity * 0.5 * time_current * time_current
