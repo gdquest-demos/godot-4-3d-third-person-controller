e72740ca93137db756cbe7f236c2f0db04c05a7a
Henrique Alves <henriquelalves@gmail.com>
2023-03-06 11:30:10 -0300

    Make mushrooms bounce player

--- JumpingPad/JumpingPad.gd
+++ JumpingPad/JumpingPad.gd
@@ -1,26 +1,14 @@
 extends Area3D
 
-@export var impulse_strenght := 8.0
-@onready var _player: CharacterBody3D = null
-
+@export var impulse_strenght := 10.0
+@onready var mushroom : Node3D = %mushroom
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-
-
-func _physics_process(_delta: float) -> void:
-	if _player:
-		var player_is_just_jumping := Input.is_action_just_pressed("jump")
-		if player_is_just_jumping:
-			_player.velocity += transform.basis * Vector3.UP * impulse_strenght
-
-
-func _on_body_entered(body: Node3D) -> void:
-	if body is Player:
-		_player = body
-
-
-func _on_body_exited(body: Node3D) -> void:
-	if body is Player:
-		_player = null
+	body_entered.connect(func(body):
+		if body is Player:
+			body.velocity = (Vector3.UP * body.jump_initial_impulse) + (transform.basis * Vector3.UP * impulse_strenght)
+			
+			var tween := create_tween()
+			mushroom.scale.y = 0.4
+			tween.tween_property(mushroom, "scale:y", 1.0, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	)
