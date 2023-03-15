fc8ac76d2817f967154dbf6b1612e578c376e5d7
Aaron Benjamin <lifeartstudios@gmail.com>
2023-03-15 15:11:36 -0400

    Fix "!material" error log issues
    BeeBot.gd had surface_material_overrides that were not released during Queue_Free()

--- Enemies/BeeBot/bee_root.gd
+++ Enemies/BeeBot/bee_root.gd
@@ -13,3 +13,8 @@ func play_spit_attack():
 
 func play_poweroff():
 	state_machine.travel("power_off")
+
+func _exit_tree(): # When someone calls queue_free() here
+	$bee_bot/Armature/Skeleton3D/bee_bot2.set("surface_material_override/1", null)
+	$bee_bot/Armature/Skeleton3D/bee_bot2.set("surface_material_override/2", null)
+	$bee_bot/Armature/Skeleton3D/bee_bot2.set("surface_material_override/3", null)
